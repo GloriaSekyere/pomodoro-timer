94e73da030bfa49c407d2fcf828cd001ab61255d
GloriaSekyere <gloriasekyere15@gmail.com>
2021-10-11 14:08:00 +0000

    add hightlight to control buttons

--- pomodoro-timer/src/components/Controls.js
+++ pomodoro-timer/src/components/Controls.js
@@ -10,10 +10,14 @@ const Controls = () => {
       before:absolute before:top-0
       md:w-24 md:max-h-12">
         Start</button>
-      <button className="bg-red-200 hover:bg-red-300 h-14 text-white font-bold rounded
+      <button className="bg-red-200 hover:bg-red-300 h-14 text-white font-bold rounded relative
+      before:empty-content before:bg-red-100 before:block before:h-0.5 before:w-full
+      before:absolute before:top-0
       md:w-24 md:max-h-12">
         Stop</button>
-      <button className="bg-gray-200 hover:bg-gray-300 h-14 text-black font-bold rounded
+      <button className="bg-gray-200 hover:bg-gray-300 h-14 text-black font-bold rounded relative
+      before:empty-content before:bg-gray-100 before:block before:h-0.5 before:w-full
+      before:absolute before:top-0
       md:w-24 md:max-h-12">
         Reset</button>
     </div>
