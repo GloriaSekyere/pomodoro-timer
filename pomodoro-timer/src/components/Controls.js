02f2bfad18c74b1b2eb122ad58baeff50882c894
GloriaSekyere <gloriasekyere15@gmail.com>
2021-10-12 11:05:26 +0000

    fix gray button color issues

--- pomodoro-timer/src/components/Controls.js
+++ pomodoro-timer/src/components/Controls.js
@@ -7,15 +7,17 @@ const Controls = () => {
       <button 
       className="bg-green-200 hover:bg-green-300 h-14 text-white font-bold rounded relative
       before:empty-content before:bg-green-100 before:block before:h-0.5 before:w-full
-      before:absolute before:top-0
+      before:absolute before:top-0 before:rounded-t-full
       md:w-24 md:max-h-12">
         Start</button>
       <button className="bg-red-200 hover:bg-red-300 h-14 text-white font-bold rounded relative
+      border-red-300
       before:empty-content before:bg-red-100 before:block before:h-0.5 before:w-full
       before:absolute before:top-0
       md:w-24 md:max-h-12">
         Stop</button>
-      <button className="bg-gray-200 hover:bg-gray-300 h-14 text-black font-bold rounded relative
+      <button className="bg-gray-200 hover:bg-gray-400 h-14 text-black font-bold rounded relative
+      border-gray-600
       before:empty-content before:bg-gray-100 before:block before:h-0.5 before:w-full
       before:absolute before:top-0
       md:w-24 md:max-h-12">
