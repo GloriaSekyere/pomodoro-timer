2201a4e01c13d380ac4c229dc0fbafd5b05003a3
GloriaSekyere <gloriasekyere15@gmail.com>
2021-10-14 15:49:35 +0000

    add borders and highlights to session and control buttons

--- pomodoro-timer/src/components/Controls.js
+++ pomodoro-timer/src/components/Controls.js
@@ -6,17 +6,20 @@ const Controls = () => {
     md:justify-between md:flex-row md:w-custom26 lg:w-custom28">
       <button 
       className="bg-green-200 hover:bg-green-300 h-14 text-white font-bold relative
-      border-black-300
+      border-solid border-1 border-green-400
+      
       before:empty-content before:bg-green-100 before:block before:h-0.5 before:w-full
-      before:absolute before:top-0 before:rounded-t-full
-
+      before:absolute before:top-0 
+      
       after:empty-content after:bg-green-300 after:block after:h-0.5 after:w-full
       after:absolute after:bottom-0
 
       md:w-24 md:max-h-12">
         Start</button>
-      <button className="bg-red-200 hover:bg-red-300 h-14 text-white font-bold relative
-      border-red-300
+      
+      <button 
+      className="bg-red-200 hover:bg-red-300 h-14 text-white font-bold relative
+      border-solid border-1 border-red-400
 
       before:empty-content before:bg-red-100 before:block before:h-0.5 before:w-full
       before:absolute before:top-0
@@ -26,8 +29,11 @@ const Controls = () => {
 
       md:w-24 md:max-h-12">
         Stop</button>
-      <button className="bg-gray-200 hover:bg-gray-400 h-14 text-black font-bold relative
-      border-gray-600
+      
+      <button 
+      className="bg-gray-200 hover:bg-gray-400 h-14 text-black font-bold relative
+      border-solid border-1 border-gray-300
+      
       before:empty-content before:bg-gray-100 before:block before:h-0.5 before:w-full
       before:absolute before:top-0
 
--- pomodoro-timer/src/components/Session.js
+++ pomodoro-timer/src/components/Session.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 function Session() {
   return (
-    <div className="py-9 px-4 divide-x divide-blue-200 flex 
+    <div className="py-9 px-4 divide-x divide-blue-300 flex 
     justify-center items-center w-full mx-auto md:w-custom50">
       <Link to="/" className="session-link">Pomodoro</Link>
       <Link to="/" className="session-link">Short Break</Link>
